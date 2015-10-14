f3a5b198a9534496dd16debd87cf6d5b1b122f1a
shivajidora <shivajidora.b@gmail.com>
2015-10-14 15:05:29 +0530

    Update waterFloat.js

--- waterFloat.js
+++ waterFloat.js
@@ -1,13 +1,27 @@
 function waterFloat(elm,t,d,v){
 var i = elm;
 var runIt = function (elm,t,d,v) {
-       elm.animate({top:'+='+v},t,"linear",function(){
-			elm.css({'-webkit-transform' : 'rotate('+d+'deg)'});
-			elm.animate({top:'-='+v},t,"linear",function(){
-				elm.css({'-webkit-transform' : 'rotate(-'+d+'deg)'});
-				runIt(elm,t,d,v);
-			});
-	   });
-   }
-   runIt(i,t,d,v);
+    elm.animate({top:'+='+v},t,"linear",function(){
+		$({deg: -d}).animate({deg: d}, {
+			duration: t,
+			step: function(now){
+				elm.css({
+					transform: "rotate(" + now + "deg)"
+				});
+			}
+		},"linear");
+		elm.animate({top:'-='+v},t,"linear",function(){
+			$({deg: d}).animate({deg: -d}, {
+			duration: t,
+			step: function(now){
+            elm.css({
+                transform: "rotate(" + now + "deg)"
+				});
+			}
+		},"linear");
+			runIt(elm,t,d,v);
+		});	
+	});
+}
+runIt(i,t,d,v);
 }
